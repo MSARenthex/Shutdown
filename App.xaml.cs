0481dbf970484958c4a625a1af518c7c076dada5
Mateus Simões <mateussimoesdearaujo@gmail.com>
2024-05-22 13:01:53 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace Shutdown
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.MinimumHeight = 650; // altura mínima da janela
+            window.MinimumWidth = 450; // largura mínima da janela
+
+            window.Width = 800;
+            window.Height = 660;
+
+            window.SizeChanged += OnWindowSizeChanged; // largura mínima da janela
+
+            return window;
+        }
+
+        private void OnWindowSizeChanged(object sender, EventArgs e)
+        {
+            // Verifica se o objeto sender é uma janela
+            if (sender is Window window)
+            {
+                if(window.Width < 800)
+                {
+                    window.MinimumHeight = 850;
+                }
+                    
+                if (window.Width >= 800)
+                {
+                    window.MinimumHeight = 650;
+                }
+            }
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shutdown.Services
+{
+    public class UserService
+    {
+        public int ValueHoras { get; set; }
+        public int ValueMinutos { get; set; }
+        public int ValueSegundos { get; set; }
+        public int acao { get; set; }
+        public int musica { get; set; }
+
+    }
+}
